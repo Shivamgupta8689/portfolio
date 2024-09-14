a77eff92b78f37e406317ac0778e046e89aa8325
shivam <shivamgupta86923@gmail.com>
2024-09-14 13:38:38 +0530

    Done!

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Shivam, a passionate Web developer with a keen eye for MERN
+          Stack .
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education
+        </h1>
+        <span>
+          Btech in computer Engineering, Lokmanya tilak college of engineering, 2nd year, University of mumbai,
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills
+        </h1>
+        <span>
+          Intermediate in java and Springboot, C programming, Python, Video editing, Photo editing, 
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Experience
+        </h1>
+        <span>
+          Completed IBM CSR Box Summer Internship 2024 on Data Analytics.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Hobbies
+        </h1>
+        <span>
+          Reading Books, Learning new things, playing Cricket
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative Web development solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
